178215d5ae33c0bf744d6431d8fa217303e92def
misham16 <mmclendon@knights.ucf.edu>
2019-06-30 17:42:37 -0400

    First commit

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ChangeName : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown("space"))
+        {
+        TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+        textmeshPro.SetText("Hello World from Misha"); 
+        }  
+    }
+}
